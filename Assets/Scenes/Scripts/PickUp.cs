e469cfc5ff12f75e000ee98d2b088078bc1e0d9d
Josue Flores <jifr126@gmail.com>
2021-09-19 20:05:47 -0500

    Module 4 Progress
    Almost done

---
+++ Assets/Scenes/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum PickupType
+{
+    crate, crystal, rock, star                 //add as needed
+}
+
+
+public class PickUp : MonoBehaviour
+{
+
+    [SerializeField]  //allows modification in inspector 
+    private int value;
+
+    //read-only property
+    public int Value
+    {
+        get { return value; }
+    }
+
+
+    public PickupType type; //what is the PickupType of this object
+
+}// end class
--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -4,50 +4,123 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //create custom data-type
-    public enum HeroState { idle = 0, walk = 1, jump = 2 } //custom data-type 
-    public HeroState currentHeroState;  //variable to keep track of current animation state
+    //V4 Score
+    public enum HeroState { idle, walk, jump }  //create custom data-type - have integer values
 
-    private bool facingRight = true; // initialize when declare (first way),  false by default
+    public HeroState currentHeroState;  //will display the current enum 
 
-    private Animator animator; //obj-reference variable to access the animator component on this gameObject
+    private Animator animator;  //- null - reference variable to access animator component
 
-    //inspector is the second way to initialize a value
+    public bool facingRight;  //keep track of sprite direction - used in Flip
+    private Rigidbody2D myRBody2D;
+    public float forceX; //used for adjusting velocity
+
+    public Transform groundCheck; //transform component on GroundCheck object
+    public LayerMask groundLayer; //allows us to interact with a physics Layer 
+    public float groundCheckRadius;  //
+    public float jumpForce;
+    public bool grounded = false;  //will let us see if the gameOjb is grounded
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-        facingRight = true; //final way to initialize, overrides all prior settings
-        animator = GetComponent<Animator>(); //<T>  //call a method - make connection to component on gameObject in Unity scene
-        animator.SetInteger("HeroState", (int)HeroState.idle);
+        currentHeroState = HeroState.idle;    //initialize to show it's in idle to start
+        animator = GetComponent<Animator>();//is on the same game object as this script
+        animator.SetInteger("HeroState", (int)HeroState.idle);  //send in the signal: 0
+        facingRight = true;
+        myRBody2D = GetComponent<Rigidbody2D>();
+        forceX = 100.0f;  //force value may need adjusted
+
+        groundCheckRadius = 0.2f; //may need modified
+        jumpForce = 10f; //may need modified
     }
 
-    //Fixed update used for physics, to give smooth motion, called at consistent time increments
-    void FixedUpdate()
+    // Update is called once per frame
+    void FixedUpdate()  //physics methods executed - want consistant time between frames
     {
-        float inputX = Input.GetAxis("Horizontal"); // key input:  -1, 0, 1
+        float inputX = Input.GetAxis("Horizontal");  //values of -1, 0 , 1 
         bool isWalking = Mathf.Abs(inputX) > 0;
+
+        if (isWalking)
+        {
+            //check for flipping
+            if (inputX > 0 && !facingRight)  //moving right, facing left
+            {
+                Flip(); //flip right
+            }
+            else if (inputX < 0 && facingRight) //moving left, facing right
+            {
+                Flip(); //flip left
+            }
+            currentHeroState = HeroState.walk;
+            animator.SetInteger("HeroState", (int)HeroState.walk);
+            myRBody2D.velocity = new Vector2(0, myRBody2D.velocity.y);  //reset the velocity to 0 //may come back and change
+            myRBody2D.AddForce(new Vector2(inputX * forceX, 0)); //add horizontal force to move the player
+        }
+        else
+        {
+            currentHeroState = HeroState.idle;
+            animator.SetInteger("HeroState", (int)HeroState.idle);
+        }
 
         //check for spacebar or w, up keys to indicate vertical jump
         bool jumpPressed = Input.GetButtonDown("Jump") || Input.GetAxis("Vertical") > 0;
 
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        if (isWalking)
+        if (jumpPressed && grounded)
         {
-            animator.SetInteger("HeroState", (int)HeroState.walk);
-        }
-        else
-        {
-            animator.SetInteger("HeroState", (int)HeroState.idle);
-        }
-
-
-        if (jumpPressed)
-        {
+            currentHeroState = HeroState.jump;
             animator.SetInteger("HeroState", (int)HeroState.jump);
+            Debug.Log("Jumping");
+
+            ///Vertical Force for Movement
+            myRBody2D.velocity = new Vector2(myRBody2D.velocity.x, 0);  //reset the y velocity to 0 //keep horizontal movement
+            myRBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //add horizontal force to move the player
+
         }
 
+    }//end FixedUpdate
+
+    //we have determined it is facing the wrong direction, it must need flipped for this to be executed
+    private void Flip()
+    {
+        facingRight = !facingRight; //toggle direction variable
+        //get Scale Vector
+        Vector3 theScale = transform.localScale;  //get the current values for Scale Vector
+        theScale.x *= -1; //modify the X component    //mirror the sprite
+        transform.localScale = theScale; //set the actual Scale vector with our temp Vector
+    }
+
+    /// <summary>
+    /// This is the start of the Event-Chain for Game Score, health to be changed
+    /// </summary>
+    /// <param name="collision">Collision.</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with something");
+        //check tag on gameObject that has this collider on it
+        if (collision.CompareTag("Collectible"))
+        {
+            //get the PickUp component from the gameObject with this collider on it
+            PickUp item = collision.GetComponent<PickUp>();
+            Debug.Log("collided with a collectible of value: " + item.Value);
+
+            GameData.instanceRef.Add(item.Value);
+            Destroy(collision.gameObject); //destroy GameObject of the object we collided with
+        }
+        else if (collision.CompareTag("Hazard"))
+        {
+            PickUp item = collision.GetComponent<PickUp>();
+            Debug.Log("collided with a hazard of value: " + item.Value);
+            //update gameData database
+
+            GameData.instanceRef.TakeDamage(item.Value);
+            Destroy(collision.gameObject); //destroy GameObject of the object we collided with
+
+        }
     }
 
 
-}  //end class PlayerController
+
+}///end of Class
